df30877f92d8816f0b1c0bda84713ee2bd6fb6bc
imranrizvi648 <de.imran@brbgroup.pk>
2026-05-13 11:20:31 +0500

    Update script source URL in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -38,7 +38,7 @@ export default function RootLayout({ children }) {
       
       
         <Script 
-          src="http://192.168.0.30:8100/widget/wid_14b6a892072541ab.js"
+          src="http://192.168.0.30:8100/widget/wid_809b418c1ae44350.js"
           strategy="lazyOnload"
         />
 
